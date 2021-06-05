f50e015529119ce249092577197ac92449f29bc0
Yuuya-oozeki <80250869+Yuuya-oozeki@users.noreply.github.com>
2021-06-05 14:59:13 +0900

    修正完了「プロトタイプの表示」「URLログイン問題』

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype,except: [:index, :new, :create]
   before_action :authenticate_user!,except:[:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -57,6 +58,6 @@ class PrototypesController < ApplicationController
   end
 
   def contributor_confirmation
-    redirect_to root_path unless current_user == @prototye.user
+    redirect_to root_path unless current_user == @prototype.user
     end
   end
